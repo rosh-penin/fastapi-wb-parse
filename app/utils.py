a1b685e4c201eb769253ccced55063bff6bd49ec
Покровский Сергей <sergee13@yandex.ru>
2023-04-17 14:31:36 +0300

    renamed id to nm_id

--- app/utils.py
+++ app/utils.py
@@ -4,16 +4,16 @@ import requests
 from models import ObjectModel
 
 
-def get_response_from_id(id: str):
+def get_response_from_id(nm_id: str):
     """Get response from url by id."""
-    response = requests.get(f'https://card.wb.ru/cards/detail?nm={id}')
+    response = requests.get(f'https://card.wb.ru/cards/detail?nm={nm_id}')
     if not response or response.status_code == 404:
         raise HTTPException(404, 'There is no such product')
 
     return response.json()
 
 
-def lazy_get(nm_id, session):
+def lazy_get(nm_id: int, session):
     """DRY getting object."""
     obj = session.get(ObjectModel, nm_id)
     if not obj:
